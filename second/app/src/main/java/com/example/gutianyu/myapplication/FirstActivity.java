c43153ab242b0a53b251eea177465776e1a650eb
xiaogugu12 <35867331+xiaogugu12@users.noreply.github.com>
2018-02-24 08:12:43 +0800

    按钮显示提示信息
    显示按钮的提示信息

--- second/app/src/main/java/com/example/gutianyu/myapplication/FirstActivity.java
+++ second/app/src/main/java/com/example/gutianyu/myapplication/FirstActivity.java
@@ -2,6 +2,9 @@ package com.example.gutianyu.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1=(Button)findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this,"You  clicked  Button  1",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
