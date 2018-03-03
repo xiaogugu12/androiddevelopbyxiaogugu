4a38b99ecce3824cd99fd6da29d17c06d09cc634
xiaogugu12 <35867331+xiaogugu12@users.noreply.github.com>
2018-03-03 12:15:41 +0800

    ListViewTest初步使用
    ListViewTest初步使用

---
+++ ListViewTest/app/src/main/java/com/example/gutianyu/listviewtest/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.gutianyu.listviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[]  data={"Apple","Banana",
+    "Orange","Watermelon","Pear","Grape","Pineapple","Strawberry",
+    "Cherry","Mango","Apple","Banana","Orange","Watermelon","Pear",
+    "Grape","Pineapple","Strawberry","Cherry","Mango"};
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String>  adapter=new ArrayAdapter<String>(
+                MainActivity.this,android.R.layout.simple_list_item_1,data);
+        //利用android.R.layout.simple.list_item_1作为ListView子项布局的id
+
+        ListView  listView=(ListView)findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+        //最后调用ListView中的setAdapter将构造好的
+        //适配器对象传递过去
+    }
+}
